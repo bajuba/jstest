ce135f43399efe102e7ba7651fc8de6cc25a32e2
James Munger <jmunger@lakelandcollege.edu>
2020-01-15 19:54:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/************************
+Creator: James Munger
+Purpose: I wish I knew.
+***********************/
+var apple = 5+4-6/4;
+
+let apple2 = 5;
+
+const tax = 7;
+
+let appleBananaOrange = "hi";
+
+console.log(apple);
+
+console.log(apple2)
